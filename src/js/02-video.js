38774477c24af602c2f21f4897ae161d4d8d3363
Anastasiia Tkachenko <96151518+Anastasiia008@users.noreply.github.com>
2023-05-31 12:58:09 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+// Робимо імпорт для контролю частоти виклику функції
+import throttle from 'lodash.throttle';
 
+// Робимо імпорт для роботи та контролю плеєра
+import Player from '@vimeo/player';
+
+// Отримуємо посилання на елемент
+const iframe = document.querySelector('iframe');
+
+// Створюємо екземпляр преєра
+const player = new Player(iframe);
+
+// Створюємо ключ для зберігання поточного часу локально
+const STORAGE_KEY = 'videoplayer-current-time';
+
+// Фіксуємо відпрацювання події
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+// Ввімкнення з часу, яке зберігли
+playOnCurrentTime();
+
+// Фунція, для запам'ятовування ті зберігання часу відео
+function onTimeUpdate(data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+}
+
+// Фунція, для відтворення відео зі збереженого часу
+function playOnCurrentTime() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    if (savedData) {
+    player.setCurrentTime(savedData);
+}
+}
